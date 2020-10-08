cc0b43114617fc4a4ac8e306300a3229e311038c
Akash Palrecha <akashpalrecha@gmail.com>
2020-10-08 21:45:22 +0530

    added rcan_4_4_vgg_x2 training slurm script

---
+++ code/scripts/slurm/train_rcan_4_4_vgg_gan_x2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -n 8
+#SBATCH -N 1
+#SBATCH -t 1-6:00
+#SBATCH -p seas_gpu
+#SBATCH --exclude=seasdgx103
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32768
+#SBATCH -o /n/home00/apalrecha/lab/Deblurring/distill-superres/experiment/slurm_output/rcan_4_4_vgg_x2_%j.out
+#SBATCH -e /n/home00/apalrecha/lab/Deblurring/distill-superres/experiment/slurm_output/rcan_4_4_vgg_x2_%j.err
+
+echo "Settinp up env. Loading Conda, MATLAB, etc ..."
+source ~/.bashrc
+cd /n/home00/apalrecha/lab
+echo "Activating EDSR conda environment ..."
+source activate envs/edsr
+echo "Setup Done!"
+cd Deblurring/distill-superres/code
+echo ""
+echo "---- BEGIN TRAINING ----"
+echo ""
+python train.py --ckp_dir overall_distillation/rcan/vgg_4_4_x2/ --scale 2 --teacher [RCAN] --model RCAN --n_resblocks 4 --n_resgroups 4 --alpha 0.5 --feature_loss_used 0 --wgan --epochs 100 --patch_size 96 --chop --d_lr 0.000005 --no_critic_clip --d_loss_weight 10.0
+echo ""
+echo "---- TRAINING COMPLETE ----"
+echo ""
