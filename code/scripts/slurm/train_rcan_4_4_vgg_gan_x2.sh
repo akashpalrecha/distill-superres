6ebc607221579a55b7052bf3f7b1344714c57e69
Akash Palrecha <akashpalrecha@gmail.com>
2020-10-10 10:53:52 +0530

    changed partition for rcan_4_4_vgg slurm script

--- code/scripts/slurm/train_rcan_4_4_vgg_gan_x2.sh
+++ code/scripts/slurm/train_rcan_4_4_vgg_gan_x2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -n 8
 #SBATCH -N 1
-#SBATCH -t 1-6:00
-#SBATCH -p seas_gpu
+#SBATCH -t 0-18:00
+#SBATCH -p seas_dgx1
 #SBATCH --exclude=seasdgx103
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32768
@@ -19,7 +19,7 @@ cd Deblurring/distill-superres/code
 echo ""
 echo "---- BEGIN TRAINING ----"
 echo ""
-python train.py --ckp_dir overall_distillation/rcan/vgg_4_4_x2/ --scale 2 --teacher [RCAN] --model RCAN --n_resblocks 4 --n_resgroups 4 --alpha 0.5 --feature_loss_used 0 --wgan --epochs 100 --patch_size 96 --chop --d_lr 0.000005 --no_critic_clip --d_loss_weight 10.0
+python train.py --ckp_dir overall_distillation/rcan/vgg_4_4_x2/ --scale 2 --teacher [RCAN] --model RCAN --n_resblocks 4 --n_resgroups 4 --alpha 0.5 --feature_loss_used 0 --wgan --epochs 100 --patch_size 96 --chop --d_lr 0.00005 --no_critic_clip
 echo ""
 echo "---- TRAINING COMPLETE ----"
 echo ""
