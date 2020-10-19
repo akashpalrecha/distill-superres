fa2f42e0dd3c9d84c47b60569da734a947c213d5
Akash Palrecha <akashpalrecha@gmail.com>
2020-10-19 14:06:54 +0530

    added rcan_6_6_flickr_plain train script

---
+++ code/scripts/slurm/train_rcan_6_6_flickr_plain.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -n 8
+#SBATCH -N 1
+#SBATCH -t 0-16:00
+#SBATCH -p seas_dgx1
+#SBATCH --exclude=seasdgx103
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32768
+#SBATCH -o /n/home00/apalrecha/lab/Deblurring/distill-superres/experiment/slurm_output/rcan_6_6_flickr_plain_x2_%j.out
+#SBATCH -e /n/home00/apalrecha/lab/Deblurring/distill-superres/experiment/slurm_output/rcan_6_6_flickr_plain_x2_%j.err
+
+echo "Settinp up env. Loading Conda, MATLAB, etc ..."
+source ~/.bashrc
+cd /n/home00/apalrecha/lab
+echo "Activating EDSR conda environment ..."
+source activate envs/edsr
+echo "Setup Done!"
+cd Deblurring/distill-superres/code
+echo ""
+echo "---- BEGIN TRAINING ----"
+echo ""
+python train.py --ckp_dir overall_distillation/rcan/rcan_6_6_flickr_plain_x2/ --scale 2 --teacher [RCAN] --model RCAN --n_resblocks 6 --n_resgroups 6 --alpha 0.5 --feature_loss_used 0 --epochs 100 --patch_size 96 --chop --data_train DIV2K+Flickr2k
+echo ""
+echo "---- TRAINING COMPLETE ----"
+echo ""
